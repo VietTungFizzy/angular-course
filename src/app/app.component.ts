ef992fe02b6615b6c1fd038b8db10920de00a119
Tung Dong Viet <fizzy@Tungs-MacBook-Air.local>
2025-11-25 09:00:58 +0700

    Initial defer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,6 +12,16 @@ export class AppComponent {
 
     courses = COURSES;
 
+    title = COURSES[0].description;
+
+    course = COURSES[0];
+
+    price = 9.991234;
+
+    rate = 0.67;
+
+    startDate = new Date(2000, 0, 1);
+
     onCourseSelected(course: Course) {
         console.log("App component - click event bubbled...", course)
     }
